3629e4da3e40aadde32b9d491ce9016985299a29
Jack Sheridan <jack.sheridan@helsinki.fi>
2018-03-06 22:39:28 +0200

    Remove dead code

--- src/test/java/com/js/huffman/perf/CompressionPerformanceTests.java
+++ src/test/java/com/js/huffman/perf/CompressionPerformanceTests.java
@@ -6,15 +6,8 @@ package com.js.huffman.perf;
  * and open the template in the editor.
  */
 import com.js.huffman.io.HuffmanCompressionPerfTester;
-import com.js.huffman.io.HuffmanDecompressionPerfTester;
-import java.util.ArrayList;
 import java.util.List;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -25,90 +18,6 @@ public class CompressionPerformanceTests {
     private List<String> results;
 
     public CompressionPerformanceTests() {
-    }
-
-//    @After
-//    public void printResults() {
-//
-//        for (String result : results) {
-//            System.out.println(result);
-//        }
-//    }
-    @Test
-    public void testEncoding100kb() {
-        final String input = "src/test/perf/lorem.txt";
-        final String output = "src/test/perf/lorem.huff";
-        HuffmanCompressionPerfTester p = new HuffmanCompressionPerfTester(input, output, 100);
-        p.run();
-        System.out.println("********\n");
-        System.out.println("Results for 100kb file: ");
-        this.results = p.getResults();
-    }
-
-    @Test
-    public void testEncoding500kb() {
-        final String input = "src/test/perf/customer-500kb.xml";
-        final String output = "src/test/perf/customer-500kb.xml.huff";
-        HuffmanCompressionPerfTester p = new HuffmanCompressionPerfTester(input, output, 100);
-        p.run();
-        System.out.println("********\n");
-        System.out.println("Results for 500kb file: ");
-        this.results = p.getResults();
-    }
-
-    @Test
-    public void testEncoding1mb() {
-        final String input = "src/test/perf/mondial-3.0-1mb.xml";
-        final String output = "src/test/perf/mondial-3.0-1mb.xml.huff";
-        HuffmanCompressionPerfTester p = new HuffmanCompressionPerfTester(input, output, 100);
-        p.run();
-        System.out.println("********\n");
-        System.out.println("Results for 1mb file: ");
-        this.results = p.getResults();
-    }
-
-    @Test
-    public void testEncoding2mb() {
-        final String input = "src/test/perf/Tf_french2.3mb.txt";
-        final String output = "src/test/perf/Tf_french2.3mb.txt.huff";
-        HuffmanCompressionPerfTester p = new HuffmanCompressionPerfTester(input, output, 100);
-        p.run();
-        System.out.println("********\n");
-        System.out.println("Results for 2mb file: ");
-        this.results = p.getResults();
-    }
-
-    @Test
-    public void testEncoding4mb() {
-        final String input = "src/test/perf/bible4.4mb.txt";
-        final String output = "src/test/perf/bible4.4mb.txt.huff";
-        HuffmanCompressionPerfTester p = new HuffmanCompressionPerfTester(input, output, 100);
-        p.run();
-        System.out.println("********\n");
-        this.results = p.getResults();
-    }
-
-    @Test
-    public void testEncoding9mb() {
-        final String input = "src/test/perf/bible8.9mb.txt";
-        final String output = "src/test/perf/bible8.9mb.txt.huff";
-        HuffmanCompressionPerfTester p = new HuffmanCompressionPerfTester(input, output, 100);
-        p.run();
-        System.out.println("********\n");
-        System.out.println("Results for 8mb file: ");
-        this.results = p.getResults();
-    }
-
-    @Test
-    public void testConstantKExponentialN() {
-        for (int i = 1; i <= 11; i++) {
-            String suffix = Integer.toString(i);
-            final String input = "src/test/perf/lorem" + suffix;
-            final String output = input + ".huff";
-            HuffmanCompressionPerfTester p = new HuffmanCompressionPerfTester(input, output, 10);
-            p.run();
-
-        }
     }
 
     @Test
--- src/test/java/com/js/huffman/perf/DecompressionPerformanceTests.java
+++ src/test/java/com/js/huffman/perf/DecompressionPerformanceTests.java
@@ -15,73 +15,9 @@ import org.junit.Test;
  */
 public class DecompressionPerformanceTests {
 
-    private List<String> results;
 
     public DecompressionPerformanceTests() {
     }
-
-//    @After
-//    public void printResults() {
-//
-//        for (String result : results) {
-//            System.out.println(result);
-//        }
-//    }
-    @Test
-    public void testEncoding100kb() {
-        final String input = "src/test/perf/lorem.huff";
-        final String output = "src/test/perf/lorem_decode";
-        HuffmanDecompressionPerfTester p = new HuffmanDecompressionPerfTester(input, output, 100);
-        p.run();
-        System.out.println("********\n");
-        System.out.println("Results for 100kb file: ");
-        this.results = p.getResults();
-    }
-
-    @Test
-    public void testEncoding500kb() {
-        final String input = "src/test/perf/customer-500kb.xml.huff";
-        final String output = "src/test/perf/customer-500kb_decode.xml";
-        HuffmanDecompressionPerfTester p = new HuffmanDecompressionPerfTester(input, output, 100);
-        p.run();
-        System.out.println("********\n");
-        System.out.println("Results for 100kb file: ");
-        this.results = p.getResults();
-    }
-
-    @Test
-    public void testEncoding1mb() {
-        final String input = "src/test/perf/mondial-3.0-1mb.xml.huff";
-        final String output = "src/test/perf/mondial-3.0-1mb_decode.xml";
-        HuffmanDecompressionPerfTester p = new HuffmanDecompressionPerfTester(input, output, 100);
-        p.run();
-        System.out.println("********\n");
-        System.out.println("Results for 1mb file: ");
-        this.results = p.getResults();
-    }
-
-    @Test
-    public void testEncoding2mb() {
-        final String input = "src/test/perf/Tf_french2.3mb.txt.huff";
-        final String output = "src/test/perf/Tf_french2.3mb_decode.txt";
-        HuffmanDecompressionPerfTester p = new HuffmanDecompressionPerfTester(input, output, 100);
-        p.run();
-        System.out.println("********\n");
-        System.out.println("Results for 2mb file: ");
-        this.results = p.getResults();
-    }
-
-    @Test
-    public void testEncoding4mb() {
-        final String input = "src/test/perf/bible4.4mb.txt.huff";
-        final String output = "src/test/plain/bible4.4mb_decode.txt";
-        HuffmanDecompressionPerfTester p = new HuffmanDecompressionPerfTester(input, output, 100);
-        p.run();
-        System.out.println("********\n");
-        this.results = p.getResults();
-    }
-
-
 
     @Test
     public void testConstantKLinearN() {
